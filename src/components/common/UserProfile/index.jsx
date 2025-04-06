03e1a749b6234f2a77d0c7a8de799b181db14818
Prashant Sachan <your-email@example.com>
2025-04-06 07:56:18 +0530

    add links to user profile dropdown menu options

--- src/components/common/UserProfile/index.jsx
+++ src/components/common/UserProfile/index.jsx
@@ -6,23 +6,40 @@ import {
 } from "@/components/ui/dropdown-menu";
 import { User } from "lucide-react";
 import React from "react";
+import { Link } from "react-router-dom";
 
 const UserProfile = () => {
   return (
-    <div>
-      <DropdownMenu>
-        <DropdownMenuTrigger asChild>
-          <User size={20} className="hidden lg:block" />
-        </DropdownMenuTrigger>
-        <DropdownMenuContent className="text-sm font-bold text-[#191919] min-w-[171px] mr-7">
-          <DropdownMenuLabel className="bg-red-100 p-4">Login</DropdownMenuLabel>
-          <DropdownMenuLabel className="p-4">Track Oder</DropdownMenuLabel>
-          <DropdownMenuLabel className="p-4">Activate Gift Card</DropdownMenuLabel>
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <User size={20} className="hidden lg:block" />
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="text-sm font-bold text-[#191919] min-w-[171px] mr-7">
+        <Link to="/login">
+          <DropdownMenuLabel className="bg-red-100 p-4">
+            Login
+          </DropdownMenuLabel>
+        </Link>
+        <Link to="/track-order">
+          <DropdownMenuLabel className="p-4">Track Order</DropdownMenuLabel>
+        </Link>
+        <Link to="/activate-gift-card">
+          <DropdownMenuLabel className="p-4">
+            Activate Gift Card
+          </DropdownMenuLabel>
+        </Link>
+
+        <Link to="/check-balance">
           <DropdownMenuLabel className="p-4">Check Balance</DropdownMenuLabel>
-          <DropdownMenuLabel className="p-4">Good Earth Registry</DropdownMenuLabel>
-        </DropdownMenuContent>
-      </DropdownMenu>
-    </div>
+        </Link>
+
+        <Link to="/registry">
+          <DropdownMenuLabel className="p-4">
+            Good Earth Registry
+          </DropdownMenuLabel>
+        </Link>
+      </DropdownMenuContent>
+    </DropdownMenu>
   );
 };
 
